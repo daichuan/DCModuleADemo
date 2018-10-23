9b811193e54da43fb3763b199464df2cb906a0b4
戴川 <1041936247@qq.com>
2018-10-23 15:41:49 +0800

    first commit

---
+++ DCModuleADemo/Classes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DCModuleA
+//
+//  Created by cheyr on 2018/10/23.
+//  Copyright © 2018 cheyr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Example/Pods/Target Support Files/DCModuleADemo/DCModuleADemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DCModuleADemo : NSObject
+@end
+@implementation PodsDummy_DCModuleADemo
+@end
